27d2512c78fba65411f2825d6216ca51201f33ef
JuliaJSJ <80544651+JuliaJSJ@users.noreply.github.com>
2022-12-15 11:08:30 -0500

    Update CreditCalculator.java

--- src/main/java/CreditCalculator.java
+++ src/main/java/CreditCalculator.java
@@ -1,1 +1,31 @@
+public class CreditCalculator {
+    public int payMonth(int sum, double percent, int period) {
+        double monthRate = (percent / 12) / 100;
+        double annuityRate = monthRate*(Math.pow(1+monthRate, period))/((Math.pow(1+monthRate, period))-1);
+        return (int) (sum*annuityRate);
+    }
 
+    public int allReturn(int sum, double percent, int period) {
+        double monthRate = (percent / 12) / 100;
+        double annuityRate = monthRate*(Math.pow(1+monthRate, period))/((Math.pow(1+monthRate, period))-1);
+        return (int) ((sum*annuityRate) * period);
+    }
+
+    public int overPay(int sum, double percent, int period) {
+        double monthRate = (percent / 12) / 100;
+        double annuityRate = monthRate*(Math.pow(1+monthRate, period))/((Math.pow(1+monthRate, period))-1);
+        return (int) ((sum*annuityRate)*period - sum);
+    }
+
+    public static void main(String[] args) {
+        int sum = 10_000_000;
+        double percent = 8.5;
+        int period = 240;
+
+        CreditCalculator calculator = new CreditCalculator();
+
+        System.out.println("Ежемесячный платеж: " + calculator.payMonth(sum,percent, period));
+        System.out.println("Общая сумма: " + calculator.allReturn(sum, percent, period));
+        System.out.println("Переплата: " + calculator.overPay(sum, percent, period));
+    }
+}
